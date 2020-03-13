97a899512b7f28978169b20d3b158e4b23ae95cb
Reformed Scribe <stmiller57@gmail.com>
2020-03-13 15:25:42 -0400

    changed button color and JavaScript comments

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var password = "";
 
-// Code for password length choices
-var pwordLength = prompt("How many characters would you like in your password?");
-if (pwordLength < 8 || pwordLength > 128) {
+// Password length
+var options = prompt("How many characters would you like in your password?");
+if (options < 8 || options > 128) {
   alert("Password length should be 8-128 characters. Please try again.");
   prompt("How many characters would you like in your password?");
 }
 else {
-  alert("You have chosen a password of " + pwordLength + " characters.");
+  alert("You have chosen a password of " + options + " characters.");
 }
 
-// Arrays for lowercase letters, uppercase letters, numbers and special characters
+// Arrays for lowercase letters, uppercase letters, numbers and special characters, array of arrays
 var lower = ["abcdefghijklmnopqrstuvwxyz".split("")];
 var upper = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")];
 var nums = ["0123456789".split("")];
 var special = ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split("")];
+var choices = [lower, upper, nums, special];
 
 // Confirms for password characters
 var lower = confirm("Would you like lowercase letters in your password?");
@@ -23,8 +25,6 @@ var upper = confirm("Would you like uppercase letters in your password?");
 var nums = confirm("Would you like numeric characters in your password?");
 var special = confirm("Would you like special characters in your password?");
 
-function generatePassword() {
-}
 
 // Write password to the #password input
 function writePassword() {
