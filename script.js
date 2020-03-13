709d0fd0e330ca59f7acd6696ce212a915e49fe2
Reformed Scribe <stmiller57@gmail.com>
2020-03-13 18:29:39 -0400

    Renamed variables and adjusted code comments

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
 // Function to generate password
 function generatePassword() {
+  // variable for characters that can be used in password
+  var charSet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz#@!%&()/"
   // Password length
-  var pwordcharacters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#@!%&()/";
   var options = prompt("How many characters would you like in your password?");
   if (options < 8 || options > 128) {
     alert("Password length should be 8-128 characters. Please try again.");
@@ -13,13 +13,14 @@ function generatePassword() {
   else {
     alert("You have chosen a password of " + options + " characters.");
   }
+  // Confirm which character types are used in password
   var lower = confirm("Would you like lowercase letters in your password?");
   var upper = confirm("Would you like uppercase letters in your password?");
   var nums = confirm("Would you like numeric characters in your password?");
   var special = confirm("Would you like special characters in your password?");
-  for (var i = 0; i < options.length; i++) {
+  for (var i = 0; i < options; i++) {
     var password = "";
-    var password = pwordcharacters[Math.floor(Math.random() * options.length)];
+    var password = charSet[Math.floor(Math.random() * options.length)];
     return password;
   }
 }
