7bf71650fe65667982a7cd845b35c167688264fe
Reformed Scribe <stmiller57@gmail.com>
2020-03-13 22:42:54 -0400

    Updated character sets, added if statements and cleaned up code

--- script.js
+++ script.js
@@ -4,24 +4,41 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   // variables for generate password function
   var password = "";
-  var charSet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz#@!%&()/"
+  var charSet = "";
+  var lowerCaseSet = "abcdefghijklmnopqrstuvwxyz";
+  var upperCaseSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numSet = "0123456789";
+  var specialSet = "#@!%&^()/";
   // Password length
-  var options = prompt("How many characters would you like in your password?");
-  var integer = parseInt(options, 10);
+  var options = prompt("How many characters do you want in your password?");
+  parseInt(options, 10)
   if (options < 8 || options > 128) {
     alert("Password length should be 8-128 characters. Please try again.");
-    prompt("How many characters would you like in your password?");
+    prompt("How many characters do you want in your password?");
   }
   else {
     alert("You have chosen a password of " + options + " characters.");
   }
-  // Confirm which character types are used in password
+  // Select character types used in password
   var lower = confirm("Would you like lowercase letters in your password?");
+  if (lower === true) {
+    charSet = charSet + lowerCaseSet;
+  }
   var upper = confirm("Would you like uppercase letters in your password?");
+  if (upper === true) {
+    charSet = charSet + upperCaseSet;
+  }
   var nums = confirm("Would you like numeric characters in your password?");
+  if (nums === true) {
+    charSet = charSet + numSet;
+  }
   var special = confirm("Would you like special characters in your password?");
+  if (special === true) {
+    charSet = charSet + specialSet;
+  }
+  // For loop to generate random characters for password
   for (var i = 0; i < options; i++) {
-    var password = charSet[Math.floor(Math.random() * options.length)];
+    var password = password + charSet[Math.floor(Math.random() * charSet.length)];
   }
   return password;
 }
